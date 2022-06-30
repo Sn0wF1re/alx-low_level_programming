4207922398ce433230a60379751c719b98b8ab3e
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-06-30 14:28:36 -0700

    Add corrections to program to have comments and right output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Prints a string using puts
+ * @string: "Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
