d23c4c8bf7d683bc8d028e1cae9a1808e5ba99d2
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-01 10:27:48 -0700

    Create program to print upper and lower case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *main - print upper case and lower case alphabet
+  *Return: 0
+  */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
