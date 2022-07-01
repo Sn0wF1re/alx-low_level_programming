4706b5fc428242c812a8d61d29af8a96578033d8
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-01 14:32:07 -0700

    Create program to print 2 double-digit combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  *main - print possible combinations of 2 double digits
+  *Return: 0
+  */
+int main(void)
+{
+	int x1, x2;
+
+	for (x1 = 0; x1 < 99; x1++)
+	{
+		for (x2 = x1 + 1; x2 <= 99; x2++)
+		{
+			putchar((x1 % 10) + '0');
+			putchar((x2 % 10) + '0');
+
+			if (x1 == 98 && x2 == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
