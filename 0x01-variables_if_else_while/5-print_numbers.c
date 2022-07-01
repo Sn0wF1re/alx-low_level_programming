7675c79aeb2354b7329310cb3f6c5f9077f6e918
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-01 12:02:51 -0700

    Create program to print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *main - print base 10 integers
+  *Return: 0
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
