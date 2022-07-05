591f486e49bb0a27a551365798c44bbd8f5572dc
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-05 14:15:25 -0700

    Correct program to fit Betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,6 @@
 #include "main.h"
 /**
  *jack_bauer - Print every minute of the day
- *@h: Hours
- *@m: Minutes
- *
  *Return: Nothing
  */
 void jack_bauer(void)
@@ -12,7 +9,7 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; m <=59; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
