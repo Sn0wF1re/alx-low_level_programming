662cc239904a1ad96c1e3ed4f73f3048e8d874b7
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-05 08:43:10 -0700

    Create program to print alphabet lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - print alphabet lower case
+ *@cha: Letters of alphabet
+ *
+ *Return: nothing
+ */
+
+void print_alphabet(void)
+{
+	char cha;
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		_putchar(cha);
+	}
+	_putchar('\n');
+}
