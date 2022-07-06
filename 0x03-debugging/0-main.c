39f675adc0d02b30215312e6b6680ae7393deb02
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-06 10:05:51 -0700

    Work with multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - test function that prints whether integer is positive or negative
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = -69;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+# 0x03-debugging
+Work on debugging tasks
