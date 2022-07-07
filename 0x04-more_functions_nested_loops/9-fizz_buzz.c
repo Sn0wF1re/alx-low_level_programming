ffef93e34ed4311afd7a98a5e42f4d4b111a93ec
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-07 10:59:44 -0700

    Create FizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+*main - Print 1-100, if divisible by 5, print Buzz, divisible by 3,
+*print Fizz, divisible by both 3 and 5, print FizzBuzz,
+*otherwise, print number
+*Return: 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x == 100)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
