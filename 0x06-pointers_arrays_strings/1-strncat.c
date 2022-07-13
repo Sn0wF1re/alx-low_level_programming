a734126b2e2b9210348f0d6dd53ae9e07f566984
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-07-13 06:34:59 -0700

    Write a function to concatenate two strings, max n-bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strncat - concatenate two strings
+ *@dest: string
+ *@src: string
+ *@n: number of bytes
+ *
+ *Return: a string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (b <= n)
+	{
+		dest[a] = src[b];
+		if (*(src + b) == '\0')
+		{
+			break;
+		}
+		a++;
+		b++;
+	}
+	return (dest);
+}
