d2e61d4ea1fa58a454d30d1dc1b109c03bdee47e
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-01 14:09:04 -0700

    Define a new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - structure of dog
+ *@name: pointer to dog's name
+ *@age: age of dog as a float
+ *@owner: pointer to dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
