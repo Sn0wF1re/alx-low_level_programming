df680851b9b68860abaab49193d67574cf7de447
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-01 16:59:36 -0700

    Fix code for function to create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,36 +10,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int x = 0, y = 0, z;
+
+	int i = 0, j = 0, k;
 	dog_t *dog2;
 
-	while (name[x] != '\0')
-	{
-		x++;
-	}
-	while (owner[y] != '\0')
-	{
-		y++;
-	}
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
 	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == NULL)
 	{
 		free(dog2);
 		return (NULL);
 	}
-	dog2->name = malloc(sizeof(dog2->name) * x);
+	dog2->name = malloc(i * sizeof(dog2->name));
 	if (dog2->name == NULL)
 	{
 		free(dog2->name);
 		free(dog2);
 		return (NULL);
 	}
-	for (z = 0; z < x; z++)
-	{
-		dog2->name[z] = name[z];
-	}
+	for (k = 0; k <= i; k++)
+		dog2->name[k] = name[k];
 	dog2->age = age;
-	dog2->owner = malloc(sizeof(dog2->owner) * y);
+	dog2->owner = malloc(j * sizeof(dog2->owner));
 	if (dog2->owner == NULL)
 	{
 		free(dog2->owner);
@@ -47,7 +42,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog2);
 		return (NULL);
 	}
-	for (z = 0; z < y; z++)
-		dog2->owner[z] = owner[z];
+	for (k = 0; k <= j; k++)
+		dog2->owner[k] = owner[k];
 	return (dog2);
 }
