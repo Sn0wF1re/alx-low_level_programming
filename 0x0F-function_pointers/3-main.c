3fbb28c378322afcabafd70c3b1441a8bea98892
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-03 15:41:47 -0700

    Fix program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,18 +18,18 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
+	result = get_op_func(argv[2]);
+
+	if (argv[2][1] != '\0' || result == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	result = get_op_func(argv[2]);
-
-	if (result == NULL)
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	a = atoi(argv[1]);
