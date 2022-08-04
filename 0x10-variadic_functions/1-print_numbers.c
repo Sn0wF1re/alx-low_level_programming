7b8ed4f11f0c25d7c1727e036bfd954c07e2897f
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-04 08:08:20 -0700

    Write function to print numbers, then a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - print numbers
+ *@separator: pointer to string between numbers
+ *@n: number of elements
+ *
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list va;
+	unsigned int i;
+
+	va_start(va, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			break;
+		}
+		printf("%d", va_arg(va, int));
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(va);
+}
