ceac830c11a18ba88229bed4ea3d4b8403b4d01f
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-16 08:40:56 -0700

    Fix function to delete node at given index in listint_t list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		temp = temp->next;
+		free(temp);
 		return (1);
 	}
 	for (i = 0; i < index - 1; i++)
