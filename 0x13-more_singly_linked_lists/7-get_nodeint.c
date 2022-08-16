e65bc68759dec1c8bef11de1f6223a1c1b9b01b1
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-16 01:32:36 -0700

    Fix code to return nth node in a listint_t list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *list = head;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-
-	for (i = 0; i < index; i++)
+	for (i = 0; list && i < index; i++)
 	{
 		list = list->next;
 	}
-	return (list);
+	return (list ? list : NULL);
 }
