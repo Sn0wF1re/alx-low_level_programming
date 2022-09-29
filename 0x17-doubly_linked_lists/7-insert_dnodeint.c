122e34462230bbd43366e7bd4180cf2c83fce309
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-09-29 09:21:03 -0700

    Fix code to insert node at given position in dlistint_t list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,34 +16,37 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	if (current)
+	else
 	{
-		while (current->prev)
-			current = current->prev;
-	}
-
-	while (current)
-	{
-		if (x == idx)
+		if (current)
 		{
-			if (current->next == NULL)
-				new_node = add_dnodeint_end(h, n);
-			else
-			{
-				new_node = malloc(sizeof(dlistint_t));
-				if (new_node)
-				{
-					new_node->n = n;
-					new_node->next = current->next;
-					current->next = new_node;
-					current->next->prev = new_node;
-					new_node->prev = current;
-				}
-			}
-			break;
+			while (current->prev)
+				current = current->prev;
 		}
-		current = current->next;
-		x++;
+
+		while (current)
+		{
+			if (x == idx)
+			{
+				if (current->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node)
+					{
+						new_node->n = n;
+						new_node->next = current->next;
+						current->next = new_node;
+						current->next->prev = new_node;
+						new_node->prev = current;
+					}
+				}
+				break;
+			}
+			current = current->next;
+			x++;
+		}
 	}
 	return (new_node);
 }
