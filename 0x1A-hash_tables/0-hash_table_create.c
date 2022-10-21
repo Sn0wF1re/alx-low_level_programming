ab0c7750037d014ad945f0123a60d1f118ee139e
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-10-21 04:01:05 -0700

    Fix code to allocate correct size of memory to hash node

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,13 +13,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 
-	if (table == NULL)
+	if (!(table))
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (table->array == NULL)
+	if (!(table->array))
 		return (NULL);
 
 	while (i < size)
