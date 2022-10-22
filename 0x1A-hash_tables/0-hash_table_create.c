4f209d23ccb8da9253e5c84f2c4323bff038eb07
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-10-22 08:00:09 -0700

    Write function to create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!(table->array))
+	{
+		free(table);
 		return (NULL);
+	}
 
 	while (i < size)
 	{
